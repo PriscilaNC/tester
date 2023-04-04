6f8193e8e4fa43fb219739b46dba4d75c9abd3a6
PriscilaNC <p.navarrete06@ufromail.cl>
2023-04-03 20:05:12 -0400

    ultimo intento

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        //aqui hay un cambio
-        
+        //aqui hay un cambio aaaaaaaaaaa
+
     }
 }
